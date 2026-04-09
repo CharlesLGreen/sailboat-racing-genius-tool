5c9b2cb59905230e6bb2f478fe66c57717e65e55
Charles Green <charles_green@msn.com>
2026-04-09 00:01:40 -0400

    Radar map: minZoom 6, initial zoom 8

--- src/server.js
+++ src/server.js
@@ -6807,15 +6807,15 @@ app.get("/forecast", requireAuth, (req, res) => {
         return;
       }
       if (!mapInited) {
-        map = L.map('radar-map', { zoomControl: true, attributionControl: true, minZoom: 3 }).setView([centerLat, centerLon], 7);
+        map = L.map('radar-map', { zoomControl: true, attributionControl: true, minZoom: 6 }).setView([centerLat, centerLon], 8);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          minZoom: 3,
+          minZoom: 6,
           maxZoom: 18,
           attribution: '&copy; OpenStreetMap contributors'
         }).addTo(map);
         mapInited = true;
       } else {
-        map.setView([centerLat, centerLon], 7);
+        map.setView([centerLat, centerLon], 8);
       }
       fetchFrames().then(function(f) {
         frames = f;
